023b9eac252a94d29bc15af14b64bac788ae3009
Binh Ngo <ngobinh0312@gmail.com>
2023-12-13 19:12:36 +0700

    finish ui sign in

--- app/src/main/java/com/petpaw/fragments/SignInFormFragment.java
+++ app/src/main/java/com/petpaw/fragments/SignInFormFragment.java
@@ -19,7 +19,7 @@ public class SignInFormFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM1 = "fd";
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
