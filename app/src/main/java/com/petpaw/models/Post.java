fbe62629f2a18d126d57ad1a7504148dd8e5f423
leQwang <s3912105@rmit.edu.vn>
2023-12-26 15:53:10 +0700

    [UPDATE] models Post, PostComment

--- app/src/main/java/com/petpaw/models/Post.java
+++ app/src/main/java/com/petpaw/models/Post.java
@@ -4,61 +4,131 @@ package com.petpaw.models;
 
 import org.w3c.dom.Comment;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Post {
     private String title;
     private String content;
-    private User author;
-    private final int id;
-    private List<String> images;
-    private List<PostComment> comments;
+    private String author;
+    private String petId;
     private int likes;
+    private List<String> images;
+    private List<String> comments;
 
-
-    private Post(Builder builder) {
-        this.title = builder.title;
-        this.content = builder.content;
-        this.author = builder.author;
-        this.id = builder.id;
-        this.images = builder.images;
-        this.comments = builder.comments;
-        this.likes = builder.likes;
+    public Post() {
+        this.images = new ArrayList<>();
+        this.comments = new ArrayList<>();
     }
 
-    public static class Builder {
-        private String title;
-        private String content;
-        private User author;
-        private final int id;
-        private List<String> images;
-        private List<PostComment> comments;
-        private int likes;
-
-        public Builder(String title, String content, User author, int id) {
-            this.title = title;
-            this.content = content;
-            this.author = author;
-            this.id = id;
-        }
-
-        public Builder images(List<String> images) {
-            this.images = images;
-            return this;
-        }
-
-        public Builder comments(List<PostComment> comments) {
-            this.comments = comments;
-            return this;
-        }
-
-        public Builder likes(int likes) {
-            this.likes = likes;
-            return this;
-        }
-
-        public Post build() {
-            return new Post(this);
-        }
+    public Post(String title, String content, String author, String petId) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.petId = petId;
+        this.images = new ArrayList<>();
+        this.comments = new ArrayList<>();
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPetId() {
+        return petId;
+    }
+
+    public void setPetId(String petId) {
+        this.petId = petId;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
+
+    public void setImages(List<String> images) {
+        this.images = images;
+    }
+
+    public List<String> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<String> comments) {
+        this.comments = comments;
+    }
+
+    //    private Post(Builder builder) {
+//        this.title = builder.title;
+//        this.content = builder.content;
+//        this.author = builder.author;
+//        this.id = builder.id;
+//        this.images = builder.images;
+//        this.comments = builder.comments;
+//        this.likes = builder.likes;
+//    }
+//
+//    public static class Builder {
+//        private String title;
+//        private String content;
+//        private User author;
+//        private final int id;
+//        private List<String> images;
+//        private List<PostComment> comments;
+//        private int likes;
+//
+//        public Builder(String title, String content, User author, int id) {
+//            this.title = title;
+//            this.content = content;
+//            this.author = author;
+//            this.id = id;
+//        }
+//
+//        public Builder images(List<String> images) {
+//            this.images = images;
+//            return this;
+//        }
+//
+//        public Builder comments(List<PostComment> comments) {
+//            this.comments = comments;
+//            return this;
+//        }
+//
+//        public Builder likes(int likes) {
+//            this.likes = likes;
+//            return this;
+//        }
+//
+//        public Post build() {
+//            return new Post(this);
+//        }
+//    }
 }
--- app/src/main/java/com/petpaw/models/PostComment.java
+++ app/src/main/java/com/petpaw/models/PostComment.java
@@ -4,45 +4,79 @@ import java.util.List;
 
 public class PostComment {
     private String content;
-    private User author;
-    private final int id;
+    private String author;
     private int likes;
-    private List<Pet> pets;
 
-    private PostComment(Builder builder) {
-        this.content = builder.content;
-        this.author = builder.author;
-        this.id = builder.id;
-        this.likes = builder.likes;
-        this.pets = builder.pets;
+    public PostComment() {
     }
 
-    public static class Builder {
-        private String content;
-        private User author;
-        private final int id;
-        private int likes;
-        private List<Pet> pets;
-
-        public Builder(String content, User author, int id) {
-            this.content = content;
-            this.author = author;
-            this.id = id;
-        }
-
-        public Builder likes(int likes) {
-            this.likes = likes;
-            return this;
-        }
-
-        public Builder pets(List<Pet> pets) {
-            this.pets = pets;
-            return this;
-        }
-
-        public PostComment build() {
-            return new PostComment(this);
-        }
+    public PostComment(String content, String author, int likes) {
+        this.content = content;
+        this.author = author;
+        this.likes = likes;
     }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+//    private final int id;
+//    private List<Pet> pets;
+
+//    private PostComment(Builder builder) {
+//        this.content = builder.content;
+//        this.author = builder.author;
+//        this.id = builder.id;
+//        this.likes = builder.likes;
+//        this.pets = builder.pets;
+//    }
+//
+//    public static class Builder {
+//        private String content;
+//        private User author;
+//        private final int id;
+//        private int likes;
+//        private List<Pet> pets;
+//
+//        public Builder(String content, User author, int id) {
+//            this.content = content;
+//            this.author = author;
+//            this.id = id;
+//        }
+//
+//        public Builder likes(int likes) {
+//            this.likes = likes;
+//            return this;
+//        }
+//
+//        public Builder pets(List<Pet> pets) {
+//            this.pets = pets;
+//            return this;
+//        }
+//
+//        public PostComment build() {
+//            return new PostComment(this);
+//        }
+//    }
 
 }
