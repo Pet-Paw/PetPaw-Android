9c0b5a922b5b47c96a6b381c1e2048db421f5ffd
Ngo Quoc Binh <s3927469@rmit.edu.vn>
2023-12-28 08:51:14 +0700

    Binh finish menu bottom nav

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -49,4 +48,7 @@ dependencies {
     implementation("com.google.firebase:firebase-storage:20.3.0")
     implementation("com.google.firebase:firebase-auth:22.3.0")
     implementation("com.google.firebase:firebase-firestore:24.10.0")
+
+    implementation("com.firebaseui:firebase-ui-auth:8.0.2")
+    implementation("com.firebaseui:firebase-ui-database:8.0.2")
 }
