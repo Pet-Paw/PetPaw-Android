a85d8074a4d9ffef7b1f40e57299294a20a8050c
leQwang <s3912105@rmit.edu.vn>
2023-12-29 23:08:25 +0700

    [UPDATE] ui of the post fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,9 +41,12 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment:2.7.6")
     implementation("androidx.navigation:navigation-ui:2.7.6")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 
     // Firebase
     implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
