5fc28101cf3ce903aab230d82815aa31c205ddea
devadaviddd <s3927112@rmit.edu.vn>
2023-12-29 23:11:00 +0700

    adding layout verification

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-
+    implementation("io.github.chaosleung:pinview:1.4.4")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
