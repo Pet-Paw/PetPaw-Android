9fdc15e71c16c32229f89470a38c50884711ad6d
dophuonglinhh <s3926823@rmit.edu.vn>
2023-12-29 21:43:48 +0700

    add profile ui

---
+++ app/src/main/java/com/petpaw/PetProfileActivity.java
@@ -0,0 +1,14 @@
+package com.petpaw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PetProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet_profile);
+    }
+}
