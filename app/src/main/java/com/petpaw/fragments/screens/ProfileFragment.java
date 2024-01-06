35dbdc464d80f25d28f9871cf926cb7115535755
dophuonglinhh <s3926823@rmit.edu.vn>
2024-01-06 18:19:56 +0700

    [pet-profile] add pet list adapter and edit pet profile ui

--- app/src/main/java/com/petpaw/fragments/screens/ProfileFragment.java
+++ app/src/main/java/com/petpaw/fragments/screens/ProfileFragment.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -34,14 +35,17 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.petpaw.R;
 import com.petpaw.activities.EditProfileActivity;
+import com.petpaw.adapters.PetListAdapter;
 import com.petpaw.adapters.PostListAdapter;
 import com.petpaw.databinding.FragmentMessagesBinding;
 import com.petpaw.databinding.FragmentProfileBinding;
+import com.petpaw.models.Pet;
 import com.petpaw.models.Post;
 import com.petpaw.models.User;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -64,6 +68,7 @@ public class ProfileFragment extends Fragment {
     private User user;
     private String uid;
     private List<Post> userPostList = new ArrayList<>();
+    private List<Pet> userPetList = new ArrayList<>();
     private List<User> userFollowingList = new ArrayList<>();
     private List<User> userFollowerList = new ArrayList<>();
 
@@ -109,14 +114,15 @@ public class ProfileFragment extends Fragment {
 
         //View view = inflater.inflate(R.layout.fragment_profile, container, false);
         binding = FragmentProfileBinding.inflate(inflater, container, false);
-
         binding.displayPosts.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 binding.displayPosts.setClickable(false);
                 binding.displayPosts.setTypeface(null, Typeface.BOLD);
+                binding.displayPosts.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.secondary_container));
                 binding.displayPets.setClickable(true);
                 binding.displayPets.setTypeface(null, Typeface.NORMAL);
+                binding.displayPets.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.white));
                 binding.postsRecyclerView.setVisibility(View.VISIBLE);
                 binding.petsRecyclerView.setVisibility(View.GONE);
             }
@@ -127,8 +133,10 @@ public class ProfileFragment extends Fragment {
             public void onClick(View v) {
                 binding.displayPets.setClickable(false);
                 binding.displayPets.setTypeface(null, Typeface.BOLD);
+                binding.displayPets.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.secondary_container));
                 binding.displayPosts.setClickable(true);
                 binding.displayPosts.setTypeface(null, Typeface.NORMAL);
+                binding.displayPosts.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.white));
                 binding.petsRecyclerView.setVisibility(View.VISIBLE);
                 binding.postsRecyclerView.setVisibility(View.GONE);
             }
@@ -143,12 +151,14 @@ public class ProfileFragment extends Fragment {
                 intent.putExtra("email", user.getEmail());
                 intent.putExtra("address", user.getAddress());
                 intent.putExtra("phone", user.getPhone());
-                startActivityForResult(intent, EDIT_PROFILE_REQUEST_CODE);
+                startActivity(intent);
+                //startActivityForResult(intent, EDIT_PROFILE_REQUEST_CODE);
             }
         });
 
         return binding.getRoot();
     }
+    /*
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -163,6 +173,7 @@ public class ProfileFragment extends Fragment {
             }
         }
     }
+     */
 
 
     @Override
@@ -245,6 +256,19 @@ public class ProfileFragment extends Fragment {
                 }
             }
         });
+    }
+
+    private void getUserPets() {
+        /*
+        Pet pet = new Pet.Builder("Daisy", 1) // Replace "Buddy" and 1 with actual name and id
+                .bio("Friendly and playful cat")
+                .images(Arrays.asList("image1.jpg", "image2.jpg")) // Replace with actual image names
+                .build();
+        userPetList.add(pet);
+        binding.petsRecyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+        binding.petsRecyclerView.setAdapter(new PetListAdapter(requireContext(), userPetList));
+         */
+
     }
 
     private void getUserFollowers() {
