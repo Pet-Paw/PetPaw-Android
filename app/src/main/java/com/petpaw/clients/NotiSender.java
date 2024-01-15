e07116253cab7f81e792cf6b4bcbc1e829b74db3
devadaviddd <s3927112@rmit.edu.vn>
2024-01-15 18:26:02 +0700

    setup fcm

---
+++ app/src/main/java/com/petpaw/clients/NotiSender.java
@@ -0,0 +1,75 @@
+package com.petpaw.clients;
+
+import android.os.Environment;
+import android.util.Log;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.annotations.concurrent.Background;
+import com.petpaw.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+
+public class NotiSender {
+    OkHttpClient client = new OkHttpClient();
+    String token;
+
+    MediaType mediaType = MediaType.parse("application/json");
+
+    JSONObject jsonNotif = new JSONObject();
+    JSONObject wholeObj = new JSONObject();
+
+    public NotiSender(String token) {
+        this.token = token;
+    }
+
+
+    @Background
+    public void sendNotification(String body) throws IOException {
+
+
+        Thread sendNotiThread =  new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    jsonNotif.put("title", token);
+                    jsonNotif.put("body", body);
+                    wholeObj.put("to", token);
+                    wholeObj.put("notification", jsonNotif);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+                RequestBody requestBody = RequestBody.create(mediaType, wholeObj.toString());
+                Request request = new Request.Builder().url("https://fcm.googleapis.com/fcm/send")
+                        .post(requestBody)
+                        .addHeader("Authorization", "Bearer AAAAPTkn49U:APA91bGZKD-oZHLxPPI6EtzwhB5VtkOmxVlqAQGRIYaymeNW59Cix0rpM3EPSuLrN_vyX_hgIP0lHJo57JrTio6Iot_HVSO0xFGyyvfyhte8b7oeUdhVHxuVdHflc1-S2UVX-WGZEKgB")
+                        .addHeader("Content-Type", "application/json").build();
+                try {
+                    Response response = client.newCall(request).execute();
+                    System.out.println(response.body().string());
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        sendNotiThread.start();
+    }
+}
